a2f331c756f2f37d4ab3383ac3d65a852ac1646c
techp0rtal <jlundstedtiii@gmail.com>
2024-09-16 19:58:26 -0400

    Commit 4

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
-question_bank = [
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
-]
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():# if quiz still has questions remaining
+    quiz.next_question()
+    #quiz.check_answer()
+
+print("You've completed the quiz!")
+#print(f"Your final score was {} {}."
+
+
+
+
+
 #print(question_bank)
 # This creates a list of question objects, each being initialized with a Q and an A
 #
@@ -11,11 +29,4 @@ question_bank = [
 #     question = Question(question["text"], question["answer"])
 #     question_bank.append(question)
 #     print(question_bank)
-
-for question in question_data:
-    question_text =question["text"]
-    question_answer = question["answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
-print(question_bank)
-
+# Why didn't this work? ^^^
