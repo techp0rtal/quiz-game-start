f1432392e668b8d55bd157cfb9c4de5ec592ab1e
techp0rtal <jlundstedtiii@gmail.com>
2024-09-16 21:57:18 -0400

    Commit 5

--- data.py
+++ data.py
@@ -1,4 +1,46 @@
 question_data = [
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+     "question": "RAM stands for Random Access Memory.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+     "question": "The Windows 7 operating system has six main editions.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+                                       "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+                                       "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+     "question": "The Python programming language gets its name from the British comedy group &quot;Monty Python.&quot;",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+     "question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+     "question": "Time on Computers is measured via the EPOX System.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+                                      "question": "Linus Torvalds created Linux and Git.", "correct_answer": "True",
+                                      "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+     "question": "In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+     "question": "The programming language &quot;Python&quot; is based off a modified version of &quot;JavaScript&quot;.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+     "question": "The Windows ME operating system was released in the year 2000.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+old_question_data = [
     {"text": "A slug's blood is green.", "answer": "True"},
     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
     {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
@@ -15,9 +57,4 @@ question_data = [
     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
 
 ]
-
-# print(question_data[0]["answer"])
-
-my_dict = {"questions": ["What is your name?", "How old are you?"]}
-print(my_dict["questions"][0])
 
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
@@ -16,7 +16,7 @@ while quiz.still_has_questions():# if quiz still has questions remaining
     #quiz.check_answer()
 
 print("You've completed the quiz!")
-#print(f"Your final score was {} {}."
+print(f"Your final score was {quiz.score}/{quiz.question_number}.")
 
 
 
